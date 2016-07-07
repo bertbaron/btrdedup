c600d0f87b436aa49bc9bbdb0ebb4a7b73dd6355
Bert Baron <bertusbaron@gmail.com>
2016-07-07 21:25:06 +0200

    playing around

--- btrfs/btrfs.go
+++ btrfs/btrfs.go
@@ -1,46 +1,51 @@
 package btrfs
 
+import (
+	//"golang.org/x/sys/unix"
+	"os"
+	"unsafe"
+	"golang.org/x/sys/unix"
 
-///* For extent-same ioctl */
-type Btrfs_ioctl_same_extent_info struct {
-	test int32
+	"syscall"
+)
+
+const (
+	btrfs_ioctl_magic = 0x94
+	btrfs_same_data_differs = 1
+)
+
+/* For extent-same ioctl */
+type btrfs_ioctl_same_extent_info struct {
+	fd             int64  /* in - destination file */
+	logical_offset uint64 /* in - start of extent in destination */
+	bytes_deduped  uint64 /* out - total # of bytes we were able to dedupe from this file */
+			      /* status of this dedupe operation:
+			       * 0 if dedup succeeds
+			       * < 0 for error
+			       * == BTRFS_SAME_DATA_DIFFERS if data differs
+			       */
+	status         int32  /* out - see above description */
+	reserved       uint32
 }
 
-//#/define BTRFS_IOCTL_MAGIC 0x94
-//
-//#define BTRFS_IOC_FILE_EXTENT_SAME _IOWR(BTRFS_IOCTL_MAGIC, 54, \
-//struct btrfs_ioctl_same_args)
-//
-//#define BTRFS_SAME_DATA_DIFFERS	1
-///* For extent-same ioctl */
-//struct btrfs_ioctl_same_extent_info {
-//int64_t fd;			/* in - destination file */
-//uint64_t logical_offset;	/* in - start of extent in destination */
-//uint64_t bytes_deduped;		/* out - total # of bytes we
-//					 * were able to dedupe from
-//					 * this file */
-///* status of this dedupe operation:
-// * 0 if dedup succeeds
-// * < 0 for error
-// * == BTRFS_SAME_DATA_DIFFERS if data differs
-// */
-//int32_t status;			/* out - see above description */
-//uint32_t reserved;
-//};
-//
-//struct btrfs_ioctl_same_args {
-//uint64_t logical_offset;	/* in - start of extent in source */
-//uint64_t length;		/* in - length of extent */
-//uint16_t dest_count;		/* in - total elements in info array */
-//uint16_t reserved1;
-//uint32_t reserved2;
-//struct btrfs_ioctl_same_extent_info info[0];
-//};
-//
-//static inline int btrfs_extent_same(int fd,
-//struct btrfs_ioctl_same_args *same)
-//{
-//return ioctl(fd, BTRFS_IOC_FILE_EXTENT_SAME, same);
-//}
-//
-//#endif	/* __BTRFS_IOCTL_H__ */
+type btrfs_ioctl_same_args struct {
+	logical_offset uint64 /* in - start of extent in source */
+	length         uint64 /* in - length of extent */
+	dest_count     uint16 /* in - total elements in info array */
+	reserved1      uint16
+	reserved2      uint32
+	info           *btrfs_ioctl_same_extent_info
+}
+
+func ioctl(fd, op, arg uintptr) error {
+	_, _, ep := unix.Syscall(unix.SYS_IOCTL, fd, op, arg)
+	if ep != 0 {
+		return syscall.Errno(ep)
+	}
+	return nil
+}
+func Btrfs_extent_same(file *os.File, same *btrfs_ioctl_same_args) int {
+	ioctl(file.Fd(), BTRFS_IOC_FILE_EXTENT_SAME, same)
+	return 0
+	//return ioctl(fd, BTRFS_IOC_FILE_EXTENT_SAME, same)
+}
