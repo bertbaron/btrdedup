0c241047daf249ea10459b06e054a130fcdf75f6
Bert Baron <bertusbaron@gmail.com>
2016-07-10 21:26:38 +0200

    Cleanup

--- main.go
+++ main.go
@@ -61,14 +61,11 @@ func dedup(filename1, filename2 string, len uint64) {
 	}
 	defer file2.Close()
 	log.Printf("Files: %v and %v", file1, file2)
-	//xtInfo := btrfs.BtrfsSameExtendInfo{}
-	//xtInfo.File = file2
-	//xtInfo.LogicalOffset = 0
+
 	same := make([]btrfs.BtrfsSameExtendInfo,0)
 	same = append(same, btrfs.BtrfsSameExtendInfo{File: file1, LogicalOffset: 0})
 	same = append(same, btrfs.BtrfsSameExtendInfo{File: file2, LogicalOffset: 0})
 	btrfs.BtrfsExtendSame(same, len)
-	//log.Printf("xtInfo: %v", xtInfo)
 }
 
 func main() {
