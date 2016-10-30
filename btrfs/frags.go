a3ff637e24804ac03e272eace6e6f01a05e00039
Bert Baron <bertusbaron@gmail.com>
2016-10-30 20:22:23 +0100

    Allocating memory for fiemap request in go. We forgot that C.malloc doesn't clear the data. Now we simply allocate a byte buffer in go and perform some unsafe pointer magic. Only question is how much guarantee we have that go doesn't gc the data too soon...

--- btrfs/frags.go
+++ btrfs/frags.go
@@ -1,7 +1,5 @@
 package btrfs
 
-import "C"
-
 import (
 	"os"
 	"unsafe"
@@ -10,6 +8,10 @@ import (
 
 const (
 	fiemapOp = 0xc020660b
+	extendBufferCount = 10
+	fiemapSize = 32
+	fiemapExtendSize = 56
+	datasize = fiemapSize + fiemapExtendSize * extendBufferCount
 )
 
 // size 56
@@ -33,29 +35,15 @@ type fiemap struct {
 				 //struct fiemap_extent fm_extents[0]; /* array of mapped extents (out) */
 }
 
-func fiemapSize(extendCount int) int {
-	return 32 + 56 * extendCount
-}
-
-// Allocates memory in C. Note that the C struct contains a dynamic array at the end, which is not possible in go,
-// therefore we return a go slice which is backed by that dynamic array in addition to the pointer to the args struct
-func allocateFiemap(extendCount int) (*fiemap, []fiemap_extent) {
-	size := C.size_t(fiemapSize(extendCount))
-	ptr := C.malloc(size)
-	args := (*fiemap)(ptr)
-
-	// Create a slice backed by the dynamic array at the end of the struct
-	infoPtr := unsafe.Pointer(uintptr(ptr) + 32)
-	extendInfo := (*[1 << 30]fiemap_extent)(infoPtr)[:extendCount]
-	return args, extendInfo
-}
-
 func PhysicalOffset(file *os.File) (uint64, error) {
-	data, extends := allocateFiemap(10)
-	defer free((unsafe.Pointer)(data))
+	var buffer [datasize]byte // FIXME Are we sure or how do we make sure that the buffer is not gc'ed too soon
+	argsPtr := (unsafe.Pointer(&buffer[0]))
+	xtPtr := (unsafe.Pointer(&buffer[fiemapSize]))
+	var data *fiemap = (*fiemap)(argsPtr)
+	var extends []fiemap_extent = (*[1 << 30]fiemap_extent)(xtPtr)[:extendBufferCount]
 	data.fm_start = 0
-	data.fm_length = 1024 * 1024 * 1024
-	data.fm_extent_count = 10
+	data.fm_length = 4 * 1024
+	data.fm_extent_count = extendBufferCount
 
 	if err := ioctl.IOCTL(file.Fd(), fiemapOp, (uintptr)((unsafe.Pointer)(data))); err != nil {
 		return 0, err
