d6042eeb023247bd123141be31087925f405a8b2
Bert Baron <bertusbaron@gmail.com>
2016-11-01 16:04:01 +0100

    Fixing sizes of dynamic arrays, making the code much nicer

--- btrfs/btrfs.go
+++ btrfs/btrfs.go
@@ -15,9 +15,9 @@ import (
 
 const (
 	sameExtendOp = 0xc0189436 // IOWR(0x94, 54, 24)
+	maxFileCount = 1024
 )
 
-// size 32
 type sameExtendInfo struct {
 	fd             int64  /* in - destination file */
 	logical_offset uint64 /* in - start of extent in destination */
@@ -26,33 +26,17 @@ type sameExtendInfo struct {
 	reserved       uint32
 }
 
-// size 24
 type sameArgs struct {
 	logical_offset uint64 /* in - start of extent in source */
 	length         uint64 /* in - length of extent */
 	dest_count     uint16 /* in - total elements in info array */
 	reserved1      uint16
 	reserved2      uint32
+	extend_info    [maxFileCount]sameExtendInfo // go doesn't support dynamic array, but since memory allocation is much much faster than deduplication we simply reserve a big pile of it
 }
 
-func sameMessageSize(fileCount int) int {
-	return 32 * fileCount - 8
-}
-
-// Allocates memory in C. Note that the C struct contains a dynamic array at the end, which is not possible in go,
-// therefore we return a go slice which is backed by that dynamic array in addition to the pointer to the args struct
-func allocate(fileCount int) (*sameArgs, []sameExtendInfo) {
-	size := C.size_t(sameMessageSize(fileCount))
-	ptr := C.malloc(size) // allocate memory for sameArgs + (n-1)*sameExtendInfo
-	args := (*sameArgs)(ptr)
-
-	// Create a slice backed by the dynamic array at the end of the struct
-	infoPtr := unsafe.Pointer(uintptr(ptr) + 24)
-	extendInfo := (*[1 << 30]sameExtendInfo)(infoPtr)[:fileCount - 1]
-	return args, extendInfo
-}
-
-func fillSameArgumentStructure(same []BtrfsSameExtendInfo, length uint64, args *sameArgs, info []sameExtendInfo) {
+func makeSameRequest(same []BtrfsSameExtendInfo, length uint64) *sameArgs {
+	var args sameArgs
 	args.logical_offset = same[0].LogicalOffset
 	args.length = length
 	args.dest_count = uint16(len(same) - 1)
@@ -60,12 +44,13 @@ func fillSameArgumentStructure(same []BtrfsSameExtendInfo, length uint64, args *
 	args.reserved2 = 0
 
 	for index, element := range same[1:] {
-		info[index].fd = int64(element.File.Fd())
-		info[index].logical_offset = element.LogicalOffset
-		info[index].bytes_deduped = 0
-		info[index].status = 0
-		info[index].reserved = 0
+		args.extend_info[index].fd = int64(element.File.Fd())
+		args.extend_info[index].logical_offset = element.LogicalOffset
+		args.extend_info[index].bytes_deduped = 0
+		args.extend_info[index].status = 0
+		args.extend_info[index].reserved = 0
 	}
+	return &args
 }
 
 type BtrfsSameExtendInfo struct {
@@ -89,9 +74,9 @@ func (result BtrfsSameResult) String() string {
 	return s
 }
 
-func makeSameResult(info []sameExtendInfo) []BtrfsSameResult {
-	results := make([]BtrfsSameResult, len(info))
-	for i, element := range info {
+func makeSameResult(args *sameArgs) []BtrfsSameResult {
+	results := make([]BtrfsSameResult, args.dest_count)
+	for i, element := range args.extend_info[:args.dest_count] {
 		var result BtrfsSameResult
 		if (element.status < 0) {
 			errMsg := C.GoString(C.strerror(C.int(-element.status))) // TODO Do we need to free this?
@@ -110,16 +95,15 @@ func BtrfsExtendSame(same []BtrfsSameExtendInfo, length uint64) ([]BtrfsSameResu
 	if len(same) < 2 {
 		log.Fatalf("Assertion error, there should be at least two files to deduplicate, found: %v", same)
 	}
-	args, info := allocate(len(same))
-	defer free((unsafe.Pointer)(args))
+	if len(same) > maxFileCount {
+		log.Fatalf("Deduplication is currently supported for at most %d files, but was %d", maxFileCount, len(same))
+	}
 
-	fillSameArgumentStructure(same, length, args, info)
+	args := makeSameRequest(same, length)
 
-	//log.Printf("IN:  args: %v, info: %v", args, info)
-	if err := ioctl.IOCTL(same[0].File.Fd(), sameExtendOp, (uintptr)((unsafe.Pointer)(args))); err != nil {
+	if err := ioctl.IOCTL(same[0].File.Fd(), sameExtendOp, uintptr(unsafe.Pointer(args))); err != nil {
 		return nil, err
 	}
-	//log.Printf("OUT: args: %v, info: %v", args, info)
 
-	return makeSameResult(info), nil
+	return makeSameResult(args), nil
 }
--- btrfs/frags.go
+++ btrfs/frags.go
@@ -9,12 +9,8 @@ import (
 const (
 	fiemapOp = 0xc020660b
 	extendBufferCount = 10
-	fiemapSize = 32
-	fiemapExtendSize = 56
-	datasize = fiemapSize + fiemapExtendSize * extendBufferCount
 )
 
-// size 56
 type fiemap_extent struct {
 	fe_logical    uint64  /* logical offset in bytes for the start of the extent from the beginning of the file */
 	fe_physical   uint64  /* physical offset in bytes for the start of the extent from the beginning of the disk */
@@ -24,7 +20,6 @@ type fiemap_extent struct {
 	fe_reserved   [3]uint32;
 }
 
-// size 32
 type fiemap struct {
 	fm_start          uint64 /* logical offset (inclusive) at which to start mapping (in) */
 	fm_length         uint64 /* logical length of mapping which userspace wants (in) */
@@ -32,25 +27,20 @@ type fiemap struct {
 	fm_mapped_extents uint32 /* number of extents that were mapped (out) */
 	fm_extent_count   uint32 /* size of fm_extents array (in) */
 	fm_reserved       uint32
-				 //struct fiemap_extent fm_extents[0]; /* array of mapped extents (out) */
+	fm_extends        [extendBufferCount]fiemap_extent // go doesn't support flexible array, so the easiest way is to fix the size with a constant
 }
 
 func PhysicalOffset(file *os.File) (uint64, error) {
-	var buffer [datasize]byte // FIXME Are we sure or how do we make sure that the buffer is not gc'ed too soon
-	argsPtr := (unsafe.Pointer(&buffer[0]))
-	xtPtr := (unsafe.Pointer(&buffer[fiemapSize]))
-	var data *fiemap = (*fiemap)(argsPtr)
-	var extends []fiemap_extent = (*[1 << 30]fiemap_extent)(xtPtr)[:extendBufferCount]
+	var data fiemap
 	data.fm_start = 0
 	data.fm_length = 4 * 1024
 	data.fm_extent_count = extendBufferCount
 
-	if err := ioctl.IOCTL(file.Fd(), fiemapOp, (uintptr)((unsafe.Pointer)(data))); err != nil {
+	if err := ioctl.IOCTL(file.Fd(), fiemapOp, uintptr(unsafe.Pointer(&data))); err != nil {
 		return 0, err
 	}
 	if data.fm_mapped_extents == 0 {
 		return 0, nil
 	}
-	return extends[0].fe_physical, nil
+	return data.fm_extends[0].fe_physical, nil
 }
-
